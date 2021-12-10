f35f017d2077550ae642fd5d4a0accbcadc4d755
Jason Tsoukalas <jasontsoukalas@icloud.com>
2021-12-10 20:48:17 +0200

    Test2 optimizations

--- src/test/java/gr/hua/dit/ds/circularQueue/Test2.java
+++ src/test/java/gr/hua/dit/ds/circularQueue/Test2.java
@@ -10,34 +10,47 @@ public class Test2 {
     @Test
     public void Test2() {
 
-        int count = 10;
-        int current = 0;
-        CircularQueue<Integer> q = new CircularQueue<>(count);
+        int capacity = 1000;
+        int next_push_element = 0;
+        int next_pop_element = 0;
+
+        CircularQueue<Integer> q = new CircularQueue<>(capacity);
 
         assertTrue(q.isEmpty());
 
         // We fill the queue right before double capacity is used
-        for (int i = 0; i < count - 1; i++) {
-            q.push(current++);
+        for (int i = 0; i < capacity - 1; i++) {
+            q.push(next_push_element++);
             assertEquals(q.size(), i + 1);
-            assertEquals(0, (int) q.first());
+            //assertEquals(0, (int) q.first());
         }
-        System.out.println("Faze1" + q.toString());
-        assertEquals(count, q.getCapacity());
+        System.out.println("Faze1" + q);
+        //assertEquals(capacity, q.getCapacity());
 
 
         // We empty the queue up to the point where only 3 elements remain
-        for (int i=q.getFront(); i < count - 0.4*count; i++) {
-            assertEquals(i, (int) q.pop());
+        for (int i = q.getFront(); i < capacity - 0.4 * capacity; i++) {
+            assertEquals(next_pop_element++, (int) q.pop());
         }
-        System.out.println("Faze2" + q.toString());
+        System.out.println("Faze2" + q);
 
         // We fill up the circular queue right before double capacity is used (extreme case rear<front)
-        for (int i = 0; i < 6; i++) {
-            q.push(current++);
+        for (int i = 0; i < 0.6 * capacity; i++) {
+            q.push(next_push_element++);
+        }
+        System.out.println("Faze3" + q);
+
+        // Push element so q will call doubleCapacity
+        q.push(next_push_element++);
+        System.out.println("Faze3b" + q);
+
+        {//Pop elements and check values
+            while (!q.isEmpty()) {
+                assertEquals(next_pop_element++, (int) q.pop());
+            }
         }
 
-        System.out.println("Faze3" + q.toString());
+        System.out.println("Faze4" + q);
     }
 
 
